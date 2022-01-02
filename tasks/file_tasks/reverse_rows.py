b9ee1eb4db9df309c6fcf6d8152125f85ab82aea
av_dovnar <dovnar.av@gmail.com>
2022-01-02 11:33:32 +0300

    Done: *Created .txt *Completed the task

--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,3 +3,19 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
+
+
+ROWS = "text.txt"
+
+
+def revert_rows(f_name: str):
+    with open(f_name) as file:
+        with open("new_text.txt", "w") as w_file:
+            for line in file:
+                temp_list = line.partition(".")[0].split()
+                temp_string = "." + " ".join(temp_list[::-1]) + "\n"
+                w_file.write(temp_string)
+
+
+if __name__ == "__main__":
+    revert_rows(ROWS)
