4933247c50bba9fd4c0a7e1647c1b70e9c2401c9
av_dovnar <dovnar.av@gmail.com>
2022-01-02 11:35:52 +0300

    Done: *Work with CSV

--- tasks/csv_tasks/population.py
+++ tasks/csv_tasks/population.py
@@ -3,3 +3,19 @@
 файла world_population.csv и функцию должна возвращать год, в котором
 наибольший процент прироста населения.
 """
+
+import csv
+
+
+PERCENT = "world_population.csv"
+
+
+def max_delta(f_name: str) -> str:
+    with open(f_name) as csvfile:
+        reader = csv.reader(csvfile)
+        percent_list = [[i[2], i[0]] for i in reader]
+        return max(percent_list[1:])[1]
+
+
+if __name__ == "__main__":
+    print(max_delta(PERCENT))
